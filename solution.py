3a0b89b3e2a66b13122a8013838b0a09d9f1963e
securitymike <63473027+securitymike@users.noreply.github.com>
2020-12-06 17:57:55 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -126,12 +126,11 @@ def get_route(hostname):
 
                 if types == 11:
                     bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recvPacket[28:28 +
-                    bytes])[0]
+                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     #Fill in start
                     #You should add your responses to your lists here
                     tracelist1.insert(-1, str(int((timeReceived - t)*1000))+"ms ")
-                    tracelist1.insert(-1, addr[0] + " ")
+                    tracelist1.insert(-1, addr[0])
                     tracelist2.append(tracelist1)
                     #Fill in end
                 elif types == 3:
@@ -140,7 +139,7 @@ def get_route(hostname):
                     #Fill in start
                     #You should add your responses to your lists here
                     tracelist1.insert(-1, str(int((timeReceived - t)*1000))+"ms ")
-                    tracelist1.insert(-1, addr[0] + " ")
+                    tracelist1.insert(-1, addr[0])
                     tracelist2.append(tracelist1) 
                     #Fill in end
                 elif types == 0:
@@ -149,7 +148,7 @@ def get_route(hostname):
                     #Fill in start
                     #You should add your responses to your lists here and return your list if your destination IP is met
                     tracelist1.insert(-1, str(int((timeReceived - t)*1000))+"ms ")
-                    tracelist1.insert(-1, addr[0] + " ")
+                    tracelist1.insert(-1, addr[0])
                     tracelist2.append(tracelist1) 
                     #Fill in end
                 else:
