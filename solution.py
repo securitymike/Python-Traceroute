88b965947de3cba853f7f4e7fe327c03ff50986b
securitymike <63473027+securitymike@users.noreply.github.com>
2020-12-12 15:18:39 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -159,7 +159,7 @@ def get_route(hostname):
                 break
             finally:
                 mySocket.close()
-        print(" ".join(tracelist1)) 
+        #print(" ".join(tracelist1)) 
     return (tracelist2) 
 
 #get_route("google.com") 
